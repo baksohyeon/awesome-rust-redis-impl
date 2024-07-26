289de2543571855dad41a49ade9f372c845d6654
Dorito <bori12370@gmail.com>
2024-07-26 14:28:48 +0900

    style: convert dubug option to true, and delete redundant logging

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
+  io::{Read, Write},
+  net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
 async fn main() {
-    println!("Logs from your program will appear here!");
+  println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
+  let listener = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
 
-    loop {
-        // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().unwrap();
-        // A new task is spawned for each inbound socket.
-        // The socket is moved to the new task and processed there.
-        tokio::spawn(async move { handle_connection_process(socket).await });
-    }
+  loop {
+      // The second item contains the IP and port of the new connection.
+      let (socket, _) = listener.accept().unwrap();
+      // A new task is spawned for each inbound socket.
+      // The socket is moved to the new task and processed there.
+      tokio::spawn(async move { handle_connection_process(socket).await });
+  }
 }
 
 pub async fn handle_connection_process(stream: TcpStream) {
   println!("accepted new connection");
-  println!("{:?}", stream);
   let mut tcp_stream = stream;
 
-    let mut buffer = [0; 1024];
+  let mut buffer = [0; 1024];
 
   loop {
       match tcp_stream.read(&mut buffer) {
@@ -42,7 +41,7 @@ pub async fn handle_connection_process(stream: TcpStream) {
       }
   }
 
-    println!("read from stream: {}", String::from_utf8_lossy(&buffer));
+  println!("read from stream: {}", String::from_utf8_lossy(&buffer));
 }
 
 
