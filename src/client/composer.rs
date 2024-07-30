478f34b3dc4cd6a3df3a3c4421223566cd24befa
Dorito <bori12370@gmail.com>
2024-07-30 16:49:46 +0900

    feat: add composer to constant static value

--- src/client/composer.rs
+++ src/client/composer.rs
@@ -1,3 +1,9 @@
+/// up to 512 MB in length
+const RESP_MAX_SIZE: i64 = 512 * 1024 * 1024;
+const CRLF_BYTES: &'static [u8] = b"\r\n";
+const NULL_BYTES: &'static [u8] = b"$-1\r\n";
+const NULL_ARRAY_BYTES: &'static [u8] = b"*-1\r\n";
+
 pub struct Composer {
     write_fn: fn(buf: BufMut) -> Result<(), Box<dyn Error>>,
     end_fn: fn(buf: BufMut) -> Result<(), Box<dyn Error>>,
@@ -5,5 +11,5 @@ pub struct Composer {
 }
 
 impl CommandComposer for Composer {
-    
+
 }
