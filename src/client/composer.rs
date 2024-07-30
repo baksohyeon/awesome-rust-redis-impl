133e8f344212a6a9d49d69e9b194aefbb70b7cab
Dorito <bori12370@gmail.com>
2024-07-30 15:15:42 +0900

    feat: add composer struct
    refer: https://github.com/redis/node-redis/blob/master/packages/client/lib/client/RESP2/composers/interface.ts

---
+++ src/client/composer.rs
@@ -0,0 +1,5 @@
+pub struct Composer {
+    write_fn: fn(buf: BufMut) -> Result<(), Box<dyn Error>>,
+    end_fn: fn(buf: BufMut) -> Result<(), Box<dyn Error>>,
+    reset_fn: fn(buf: BufMut) -> Result<(), Box<dyn Error>>,
+}
