70785df8fa4b50d406745355c83ac68374c55a88
Dorito <bori12370@gmail.com>
2024-07-30 15:34:19 +0900

    feat: add RespReply enum which is represents a RESP value

--- src/client/resp_codec.rs
+++ src/client/resp_codec.rs
@@ -19,6 +19,22 @@ enum ASCII {
     ZERO = 48,
     MINUS = 45
 }
+/// Represents a RESP value, see [Redis Protocol specification](http://redis.io/topics/protocol).
+// refered source code: https://github.dev/iorust/resp/tree/master/src
+#[derive(Clone, Eq, PartialEq, Debug)]
+enum RespReply {
+    SimpleString(String), // For Simple Strings the first byte of the reply is "+".
+    Error(String), // For Errors the first byte of the reply is "-".
+    Integer(i64), // For Integers the first byte of the reply is ":".
+    BulkString(String), // For Bulk Strings the first byte of the reply is "$".
+    BinaryBulkString(Vec<u8>), // For Bulk <binary> Strings the first byte of the reply is "$".
+    Null, // Null bulk reply, `$-1\r\n`
+    NullArray, // Null array reply, `*-1\r\n`
+    Array(Vec<RespReply>), // For Arrays the first byte of the reply is "*".
+}
+
+
+
 
 // TODO: make this more generic
 impl RespCodec {
