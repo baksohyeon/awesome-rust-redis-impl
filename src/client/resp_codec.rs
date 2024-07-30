ba69dece056a3bedd6a3ea8aca76f77ac52b77a2
Dorito <bori12370@gmail.com>
2024-07-30 16:48:45 +0900

    fix: delete RespCodec module
    only encode logic is implemented

--- src/client/resp_codec.rs
+++ src/client/resp_codec.rs
@@ -1,86 +1,10 @@
-pub struct RespCodec {
-    write_fn: fn(chunk: BufMut) -> Result<(), Box<dyn Error>>,
-    flush_fn: fn() -> Result<(), Box<dyn Error>>,
-}
-
-pub const CRLF: &str = "\r\n";
-
 
 // RESP2 specification
 // https://redis.io/topics/protocol
 
 
 
-enum Types {
-    SIMPLE_STRING = 43, // +
-    ERROR = 45, // -
-    INTEGER = 58, // :
-    BULK_STRING = 36, // $
-    ARRAY = 42 // *
-}
-
-enum ASCII {
-    CR = 13, // \r
-    ZERO = 48,
-    MINUS = 45
-}
-/// Represents a RESP value, see [Redis Protocol specification](http://redis.io/topics/protocol).
-// refered source code: https://github.dev/iorust/resp/tree/master/src
-#[derive(Clone, Eq, PartialEq, Debug)]
-enum RespValue {
-    SimpleString(String), // For Simple Strings the first byte of the reply is "+".
-    Error(String), // For Errors the first byte of the reply is "-".
-    Integer(i64), // For Integers the first byte of the reply is ":".
-    BulkString(String), // For Bulk Strings the first byte of the reply is "$".
-    BinaryBulkString(Vec<u8>), // For Bulk <binary> Strings the first byte of the reply is "$".
-    Null, // Null bulk reply, `$-1\r\n`
-    NullArray, // Null array reply, `*-1\r\n`
-    Array(Vec<RespValue>), // For Arrays the first byte of the reply is "*".
-}
-
-// refered source code: https://github.dev/iorust/resp/tree/master/src
-impl RespValue {
-
-    /// Returns `true` if the value is a `Null` or `NullArray`. Returns `false` otherwise.
-    /// # Examples
-    /// ```
-    /// # use self::client::{RespValue};
-    /// assert_eq!(RespValue::Null.is_null(), true);
-    /// assert_eq!(RespValue::NullArray.is_null(), true);
-    /// assert_eq!(RespValue::Integer(123).is_null(), false);
-    /// ```
-    pub fn is_null(&self) -> bool {
-        // TODO: self 와 *self 의 차이
-        match *self { 
-            RespValue::Null | RespValue::NullArray => true,
-            _ => false
-        }
-    }
-
-    /// Returns `true` if the value is a `Error`. Returns `false` otherwise.
-    /// # Examples
-    /// ```
-    /// # use self::resp::{Value};
-    /// assert_eq!(Value::Null.is_error(), false);
-    /// assert_eq!(Value::Error("".to_string()).is_error(), true);
-    /// ```
-    pub fn is_error(&self) -> bool {
-        match *self {
-            RespValue::Error(_) => true,
-            _ => false
-        }
-    }
-
-}
-
-
-
-// TODO: make this more generic
-impl RespCodec {
-    fn encode(&self, value: Value) -> Result<(), Box<dyn Error>> {
-        todo!()
-    }
-}
+pub const CRLF: &str = "\r\n";
 
 struct Encoder {
     encode_simple_string_fn: fn(value: &str) -> Result<(), Box<dyn Error>>,
