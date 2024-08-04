2be8d1baf94eed754a430e68b8658d5594dc90f4
Dorito <80398645+baksohyeon@users.noreply.github.com>
2024-08-05 00:49:02 +0900

    [Refactor]: Modularize Client Connection Handling for Improved Structure (#7)

---
+++ src/client/connection.rs
@@ -0,0 +1,41 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+pub trait Connection {
+    fn new(options: RedisConfig) -> Self;
+    fn create_listener(&self) -> TcpListener;
+    fn create_stream(&self, listener: TcpListener) -> TcpStream;
+}
+
+pub struct RedisConfig {
+    pub host: String,
+    pub port: i16,
+}
+
+pub struct ConnectionImpl {
+    host: String,
+    port: i16,
+}
+
+impl Connection for ConnectionImpl {
+    fn new(options: RedisConfig) -> Self {
+        Self {
+            host: options.host,
+            port: options.port,
+        }
+    }
+
+    fn create_listener(&self) -> TcpListener {
+        let address = format!("{}:{}", self.host, self.port);
+        println!("Creating listener, {}", address);
+        TcpListener::bind(address).expect("Failed to bind to socket")
+    }
+
+    fn create_stream(&self, listener: TcpListener) -> TcpStream {
+        loop {
+            let (socket, _) = listener.accept().expect("Failed to accept connection");
+            println!("Connection accepted: {:?}", socket);
+            return socket;
+        }
+    }
+}
