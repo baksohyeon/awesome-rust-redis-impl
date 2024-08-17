a8f1d36a0ac1876fbd037ab935568cc027c5a1d4
Dorito <bori12370@gmail.com>
2024-08-17 15:20:40 +0900

    refactor: simplifying the expiration check logic in get

--- src/client/cache_store.rs
+++ src/client/cache_store.rs
@@ -23,14 +23,16 @@ impl CacheStore {
         let expires_at = expiry.map(|expiry| Instant::now() + expiry);
         self.data.insert(key, CacheValue { value, expires_at });
     }
-
     pub fn get(&self, key: &str) -> Option<String> {
-        self.data
-            .get(key)
-            .and_then(|cache_value| match cache_value.expires_at {
-                Some(expiry) if expiry > Instant::now() => Some(cache_value.value.clone()),
-                None => Some(cache_value.value.clone()),
-                _ => None,
-            })
+        self.data.get(key).and_then(|cache_value| {
+            if cache_value
+                .expires_at
+                .map_or(true, |expiry| expiry > Instant::now())
+            {
+                Some(cache_value.value.clone())
+            } else {
+                None
+            }
+        })
     }
 }
