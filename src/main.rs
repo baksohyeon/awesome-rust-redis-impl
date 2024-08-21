73ace876c32177eb7292605f54f19a43908c42dc
Dorito <80398645+baksohyeon@users.noreply.github.com>
2024-08-21 23:49:36 +0900

    [Replication] feat: add feature configuring listening port in cmd user input (#11)
    - args: ["/tmp/codecrafters-redis-target/release/redis-starter-rust", "--port", "6389"]
    - https://redis.io/docs/latest/operate/oss_and_stack/management/replication/

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod client;
 
+use std::env;
+
 use client::connection::RedisServer;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("Redis 서버 시작 중...");
 
-    let server = RedisServer::new("127.0.0.1".to_string(), 6379);
+    let port: u16 = env::args().nth(2).unwrap_or("6379".to_string()).parse().unwrap();
+
+
+    let server = RedisServer::new("127.0.0.1".to_string(), port);
     server.run().await
 }
